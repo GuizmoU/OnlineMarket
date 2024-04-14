b33f2f2a8e4537d1a51ea29eb758e7ed6498f9bb
GuizmoU <massimo.binetruy@gmail.com>
2024-04-14 15:57:20 +0200

    Mise en place de la création d'un compte + sécurisation

---
+++ includes/model/createaccount_model.php
@@ -0,0 +1,35 @@
+<?php
+
+
+function user_exists($username, $pdo) {
+    // query
+    $query = "SELECT * FROM users WHERE username=:username";
+
+    // sécuriser le query en créant un statement
+    $stmt = $pdo->prepare($query);
+
+    // Appliquer les valeurs
+    $stmt->bindParam(":username", $username);
+
+    // executer le statement
+    $stmt->execute();
+
+    // récuperer le résultat du query
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result;
+}
+
+function create_user($username, $pwd, $pdo) {
+    // query
+    $query = "INSERT INTO users (username, pwd) VALUES (:username, :pwd);";
+
+    // sécuriser le query en créant un statement
+    $stmt = $pdo->prepare($query);
+
+    // Appliquer les valeurs
+    $stmt->bindParam(":username", $username);
+    $stmt->bindParam(":pwd", $pwd);
+
+    // executer le statement
+    $stmt->execute();
+}
