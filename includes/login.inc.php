a907da899c00ce7fb85ad19a3315c34d92544bc9
GuizmoU <massimo.binetruy@gmail.com>
2024-04-21 14:35:57 +0200

    Interface de connexion créer

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,1 +1,7 @@
 <?php
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    echo "Connecting...";
+} else {
+    header("Location: ../pages/Login.php");
+}
--- pages/Login.php
+++ pages/Login.php
@@ -6,6 +6,13 @@
     <title>Se connecter</title>
 </head>
 <body>
-    
+   <form action="../includes/login.inc.php" method="post">
+        <label for="username">Nom d'utilisateur</label>
+        <input type="text" id="username" name="username">
+        <label for="password">Mot de passe</label>
+        <input type="text" id="password" name="password">
+        <button type="submit">Se connecter</button>
+   </form> 
+   <a href="./Signup.php">Créer un compte</a>
 </body>
 </html>
