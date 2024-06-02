c8739debca948f5f50a16d07fddb20c6537bf16b
GuizmoU <massimo.binetruy@gmail.com>
2024-06-02 19:12:16 +0200

    Correction d'un bug d'affichage d'erreur

--- includes/view/login_view.inc.php
+++ includes/view/login_view.inc.php
@@ -3,5 +3,6 @@
 function handle_error() {
      if (isset($_SESSION["error"])) {
           echo("<p class='error'>" . $_SESSION["error"]["login"] . "</p>");
+          unset($_SESSION["error"]);
      }
 }
--- includes/view/signup_view.inc.php
+++ includes/view/signup_view.inc.php
@@ -4,6 +4,7 @@
 function handle_error() {
      if (isset($_SESSION["error"])) {
           echo("<p class='error'>" . $_SESSION["error"]["signup"] . "</p>");
+          unset($_SESSION["error"]);
      }
 }
 
